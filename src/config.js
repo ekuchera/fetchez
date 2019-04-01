b6d159b5c9b323c86542f600ec7668bfda0ec385
eugene <eugene.kuchera@gmail.com>
2019-04-01 11:59:38 +0200

    Config function

---
+++ src/config.js
@@ -0,0 +1,31 @@
+/**
+ * Transforms the given config
+ * method replaced by GET if absent
+ * if auth = true, adds the token authorization header
+ * if json is not false, adds the json content type
+ * (By default, considers that we are using json)
+ * If body provided, it will automatically be
+ * stringified to JSON if json is not set to false
+ * @param {dict} config
+ */
+function makeConfig(config, getToken) {
+  // Keeping headers defined by the user.
+  const headers = { ...config.headers };
+  const { auth, json, body } = config;
+
+  // Adding json Content Type header
+  if (json !== false) headers["Content-Type"] = "application/json";
+
+  // if has auth, adding the authorization header
+  if (auth && getToken) headers.Authorization = getToken();
+
+  const newConfig = {
+    method: config.method || "GET",
+    headers
+  };
+
+  if (body)
+    newConfig.body = json === false ? body : JSON.stringify(config.body);
+
+  return newConfig;
+}
--- src/index.js
+++ src/index.js
@@ -1,34 +1,5 @@
 const fetchHelper = require("./fetch");
-
-/**
- * Transforms the given config
- * method replaced by GET if absent
- * if auth = true, adds the token authorization header
- * if json is not false, adds the json content type
- * @param {dict} config
- */
-function fetchConfigHelper(config) {
-  const headers = {
-    ...config.headers
-  };
-
-  if (config.json !== false) headers["Content-Type"] = "application/json";
-
-  if (config.auth) {
-    const token = Auth.getToken();
-
-    headers.Authorization = `JWT ${token}`;
-  }
-
-  const newConfig = {
-    method: config.method || "GET",
-    headers
-  };
-
-  if (config.body) newConfig.body = JSON.stringify(config.body);
-
-  return newConfig;
-}
+const makeConfig = require("./config");
 
 function apiRestLoadAllPagesRec(url, config, prevData, resolve, reject) {
   fetchHelper(
